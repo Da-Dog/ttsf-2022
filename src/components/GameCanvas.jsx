a01950e94d4380237a88fafeedc201c198953b80
Jason Wu <jasonwu700@gmail.com>
2022-10-08 16:25:54 -0700

    updated basic

--- src/components/GameCanvas.jsx
+++ src/components/GameCanvas.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRef, useState, useEffect } from "react";
-import {drawMap, drawBorder} from "../perlin_noise/generateMap";
+import { drawMap, drawBorder } from "../perlin_noise/generateMap";
 
 const IGNITE_RATE = {
 	forest: 2,
@@ -11,148 +11,202 @@ const IGNITE_RATE = {
 const SPREAD_RATE = {
 	forest: 2,
 	grass: 0.5,
-	dryGrass: 2.5,
+	dryGrass: 2.2,
 };
 
-const GameCanvas = ({ timer, temperature }) => {
-	let new_igRate = Object.assign({}, IGNITE_RATE);
-	let new_spRate = Object.assign({}, SPREAD_RATE);
-	for (let key in IGNITE_RATE) {
-		new_igRate[key] *= temperature / 75;
-	}
-
+const GameCanvas = () => {
+	// set states and refs
 	const canvasElem = useRef(null);
 	const [gameMap, setMap] = useState([]);
+	const [temperature, setTemp] = useState(0);
+	const [timer, setTime] = useState(0);
+	const [speed, setSpeed] = useState(1);
+	const [score, setScore] = useState(0);
+  const [message, setMessage] = useState("");
 
+	// set constants
+	let new_igRate = Object.assign({}, IGNITE_RATE);
+
+	// initialize app
+	useEffect(() => {
+		const mapArr = drawMap(canvasElem.current);
+		setMap(mapArr);
+	}, []);
+
+	useEffect(() => {
+		if (gameMap.length != 0) {
+			let time = 0;
+			let myTimer = setInterval(() => {
+				time += 0.1 * speed;
+				setTime(Math.round(time * 100) / 100);
+				setTemp(temperature < 120 ? Math.floor(time / 20) + 75 : 120);
+				runOnTime();
+			}, 100);
+			let spreadTimer = setInterval(() => {
+				for (let row = 0; row < gameMap.length; row++) {
+					for (let col = 0; col < gameMap[row].length; col++) {
+						if (gameMap[row][col].onFire) {
+							spreadFire(row, col);
+						}
+					}
+				}
+			}, 700);
+			return function clearTime() {
+				clearInterval(myTimer);
+				clearInterval(spreadTimer);
+			};
+		}
+	}, [gameMap]);
+
+	useEffect(() => {
+		if (temperature >= 75) {
+			for (let key in IGNITE_RATE) {
+				new_igRate[key] *= temperature / 75;
+			}
+		}
+	}, [temperature]);
+
+  useEffect(() => {
+    if (score == 20) {
+      console.log("Helicopter")
+    }
+  }, [score])
+
+  function display_message(message) {
+    
+  }
+
+	function getNeighbors(row, col) {
+		const neighborTiles = [
+			col < 31 ? gameMap[row][col + 1] : gameMap[row][col],
+			col > 0 ? gameMap[row][col - 1] : gameMap[row][col],
+			row < 31 ? gameMap[row + 1][col] : gameMap[row][col],
+			row > 0 ? gameMap[row - 1][col] : gameMap[row][col],
+		];
+		return neighborTiles;
+	}
+
+	function spreadFire(row, col) {
+		const neighbors = getNeighbors(row, col);
+
+		for (let tile of neighbors) {
+			if (tile == null) {
+				return;
+			}
+			let fireTile;
+			let chance = Math.round(Math.random() * 100) / 100;
+			if (!tile.onFire) {
+				if (chance <= SPREAD_RATE[tile.type] / 100) {
+					fireTile = tile;
+				}
+			}
+			if (fireTile != null) {
+				fireTile["onFire"] = true;
+				setOnFire(fireTile);
+			}
+		}
+	}
 
 	function setOnFire(tile) {
 		const ctx = canvasElem.current.getContext("2d");
-    drawBorder(tile.x, tile.y, tile.pixel_size, tile.pixel_size,"hsl(30, 82%, 56%)", ctx )
+		drawBorder(
+			tile.x,
+			tile.y,
+			tile.pixel_size,
+			tile.pixel_size,
+			"hsl(30, 82%, 56%)",
+			ctx
+		);
 		ctx.fillStyle = "rgb(235, 143, 52)";
 		ctx.fillRect(tile.x, tile.y, tile.pixel_size, tile.pixel_size);
 	}
 
 	function waterOnFire(event) {
-    let x = parseInt(event.nativeEvent.offsetX/gameMap[0][0].pixel_size);
-    let y = parseInt(event.nativeEvent.offsetY/gameMap[0][0].pixel_size);
-    let tile = gameMap[x][y];
-    tile.onFire = false;
-    const ctx = canvasElem.current.getContext("2d");
-    if (tile.type == "grass") {
-      ctx.fillStyle = `hsl(75, 50%, 39%)`;
-    } else if (tile.type == "forest") {
-      ctx.fillStyle = `hsl(75, 55%, 23%)`;
-    } else {
-      ctx.fillStyle = `hsl(41, 60%, 60%)`;
-    }
-		ctx.fillRect(tile.x, tile.y, tile.pixel_size, tile.pixel_size);
+		let x = parseInt(event.nativeEvent.offsetX / gameMap[0][0].pixel_size);
+		let y = parseInt(event.nativeEvent.offsetY / gameMap[0][0].pixel_size);
+		let tile = gameMap[y][x];
+		if (tile.onFire != false) {
+			tile.onFire = false;
+			const ctx = canvasElem.current.getContext("2d");
+			let color;
+			if (tile.type == "grass") {
+				color = `hsl(75, 50%, 39%)`;
+			} else if (tile.type == "forest") {
+				color = `hsl(75, 55%, 23%)`;
+			} else {
+				color = `hsl(41, 60%, 60%)`;
+			}
 
+			drawBorder(
+				tile.x,
+				tile.y,
+				tile.pixel_size,
+				tile.pixel_size,
+				color,
+				ctx
+			);
+			ctx.fillStyle = color;
+			ctx.fillRect(tile.x, tile.y, tile.pixel_size, tile.pixel_size);
+      setScore(score + 1);
+		}
 	}
 
-	setTimeout(() => {
+	const runOnTime = () => {
 		const flatten_arr = gameMap.flat();
 		const randomTile =
 			flatten_arr[Math.floor(Math.random() * flatten_arr.length)];
 
-		const chance = Math.round(Math.random() * 100) / 100;
+		const chance = (Math.round(Math.random() * 50) + 1) / 100;
 		let fireTile;
-		switch (randomTile.type) {
-			case "grass":
-				if (chance <= new_igRate["grass"] / 100 && !randomTile.onFire) {
-					fireTile = randomTile;
-				}
-				break;
-			case "dryGrass":
-				if (
-					chance <= new_igRate["dryGrass"] / 100 &&
-					!randomTile.onFire
-				) {
-					fireTile = randomTile;
-				}
-				break;
-			case "forest":
-				if (
-					chance <= new_igRate["forest"] / 100 &&
-					!randomTile.onFire
-				) {
-					fireTile = randomTile;
-				}
-				break;
+		if (chance <= new_igRate[randomTile.type] / 100 && !randomTile.onFire) {
+			fireTile = randomTile;
 		}
-		if (fireTile != null) {
+
+		if (fireTile) {
 			fireTile["onFire"] = true;
 			setOnFire(fireTile);
-			// console.log(fireTile, 'is on fire')
 		}
+	};
 
-    function getNeighbors(row, col) {
-      const neighborTiles = [
-				col < 31 ? gameMap[row][col + 1] : gameMap[row][col],
-				col > 0 ? gameMap[row][col - 1] : gameMap[row][col],
-				row < 31 ? gameMap[row + 1][col] : gameMap[row][col],
-				row > 0 ? gameMap[row - 1][col] : gameMap[row][col],
-			];
-      return neighborTiles
-    }
-
-		function spreadFire(row, col) {
-			
-      const neighbors = getNeighbors(row, col)
-
-			for (let tile of neighbors) {
-				if (tile == null) {
-					return;
-				}
-				let fireTile;
-				let chance = Math.round(Math.random() * 100) / 100;
-				if (tile.type === "grass" && !tile.onFire) {
-					if (chance <= new_spRate["grass"] / 100) {
-						fireTile = tile;
-					}
-				} else if (tile.type === "dryGrass" && !tile.onFire) {
-					if (chance <= new_spRate["dryGrass"] / 100) {
-						fireTile = tile;
-					}
-				} else if (tile.type === "forest" && !tile.onFire) {
-					if (chance <= new_spRate["forest"] / 100) {
-						fireTile = tile;
-					}
-				}
-				if (fireTile != null) {
-					fireTile["onFire"] = true;
-					setOnFire(fireTile);
-					// console.log(fireTile, "is on fire");
-				}
-			}
-		}
-    for (let row = 0; row < gameMap.length; row++) {
-      for (let col = 0; col < gameMap[row].length; col++) {
-        if (gameMap[row][col].onFire) {
-          spreadFire(row, col)
-        }
-      }
-    }
-    // console.log(gameMap)
-	}, 0);
-
-
-  useEffect(() => {
-		const mapArr = drawMap(canvasElem.current);
-		setMap(mapArr);
-    const spreadTimer = setInterval(()=> {
-      
-    }, 100)
-	}, []);
-
-
+	const increaseSpeed = (e) => {
+		e.target.disabled = speed + 0.5 > 2.5 ? "true" : false;
+		document.getElementById("decSpeed").disabled = false;
+		let newSpeed = speed + 0.5;
+		setSpeed(newSpeed);
+		console.log(speed);
+	};
+	const decreaseSpeed = (e) => {
+		e.target.disabled = speed - 0.5 <= 0.5 ? "true" : false;
+		document.getElementById("incSpeed").disabled = false;
+		let newSpeed = speed - 0.5;
+		setSpeed(newSpeed);
+		console.log(speed);
+	};
 
 	return (
-		<div>
+		<div className="container">
 			<canvas
 				ref={canvasElem}
 				className="gameCanvas"
 				onClick={waterOnFire}
 			></canvas>
+      <div className="msg">
+        <h2>{ message }</h2>
+      </div>
+			<div className="info">
+				<h2>Time: {timer}</h2>
+				<h2>Temperature: {temperature}</h2>
+				<h2>Speed: {speed}</h2>
+				<h2>Score: {score}</h2>
+			</div>
+			<div className="controls">
+				<button onClick={increaseSpeed} id="incSpeed">
+					+ Speed
+				</button>
+				<button onClick={decreaseSpeed} id="decSpeed">
+					- Speed
+				</button>
+			</div>
 		</div>
 	);
 };
