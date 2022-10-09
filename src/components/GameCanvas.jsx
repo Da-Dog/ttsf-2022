548776b32dfb600bfe44fe1579415f49407a6519
Da_Dog <jiawenli233@outlook.com>
2022-10-08 19:32:48 -0700

    add game over counter

--- src/components/GameCanvas.jsx
+++ src/components/GameCanvas.jsx
@@ -2,15 +2,11 @@ import React, {useEffect, useRef, useState} from "react";
 import {drawBorder, drawMap} from "../perlin_noise/generateMap";
 
 const IGNITE_RATE = {
-    forest: 2,
-    grass: 5,
-    dryGrass: 10,
+    forest: 2, grass: 5, dryGrass: 10,
 };
 
 const SPREAD_RATE = {
-    forest: 4,
-    grass: 1,
-    dryGrass: 4.4,
+    forest: 4, grass: 1, dryGrass: 4.4,
 };
 
 const GameCanvas = () => {
@@ -32,59 +28,75 @@ const GameCanvas = () => {
         setMap(mapArr);
     }, []);
 
-	// spread timer
     useEffect(() => {
+        if (gameMap.length !== 0) {
+            let spreadTimer = setInterval(() => {
+                for (let row = 0; row < gameMap.length; row++) {
+                    for (let col = 0; col < gameMap[row].length; col++) {
+                        if (gameMap[row][col].onFire) {
+                            spreadFire(row, col);
+                        }
+                    }
+                }
+            }, 700 / speed);
+            let gameOverCounter = setInterval(() => {
+                let counter = 0;
 
-		let spreadTimer = setInterval(() => {
-			for (let row = 0; row < gameMap.length; row++) {
-				for (let col = 0; col < gameMap[row].length; col++) {
-					if (gameMap[row][col].onFire) {
-						spreadFire(row, col);
-					}
-				}
-		}}, 1000 / speed)
-			
-		return function clearTime() {
-			clearInterval(spreadTimer);
-			// clear timeout of every tiles
-			const flatten_arr = gameMap.flat();
-			for (let tile of flatten_arr) {
-				if (tile["burnCd"]) {
-					clearInterval(tile['burnCd'])
-				}
-			}
-		};
+                for (let row = 0; row < gameMap.length; row++) {
+                    for (let col = 0; col < gameMap[row].length; col++) {
+                        if (gameMap[row][col].onFire === true) {
+                            counter++;
+                        } else if (gameMap[row][col].isDead === true) {
+                            counter++;
+                        }
+                    }
+                }
+            }, 1000);
+            return function clearTime() {
+                clearInterval(spreadTimer);
+                clearInterval(gameOverCounter);
+                // clear timeout of every tiles
+                const flatten_arr = gameMap.flat();
+                for (let tile of flatten_arr) {
+                    if (tile["burnCd"]) {
+                        clearInterval(tile['burnCd'])
+                    }
+                }
+            };
+        }
     }, [gameMap, speed]);
 
-	// general timer and lightning
     useEffect(() => {
         let time = timer;
         let myTimer = setInterval(() => {
             time = time + 0.1 * speed;
             setTime((Math.round(time * 10) / 10));
-            setTemp(temperature < 120 ? Math.floor(time / 10) + 75 : 120);
+            setTemp(temperature < 120 ? Math.floor(time / 20) + 75 : 120);
             runOnTime();
         }, 100);
-        let thunder = setInterval(() => {
-			let chance = Math.random() * 100
-			if (chance < 3) {
-				const flatten_arr = gameMap.flat();
-				let fireTile = flatten_arr[Math.floor(Math.random() * flatten_arr.length)];
-				
-				if (fireTile) {
-					fireTile["onFire"] = true;
-					setOnFire(fireTile);
-					for (let tile of getNeighbors(fireTile["x"] / fireTile["pixel_size"], fireTile["y"] / fireTile["pixel_size"])) {
-						setOnFire(tile);
-						gameMap[tile.y / fireTile["pixel_size"]][tile.x / fireTile["pixel_size"]]["onFire"] = true;
-					}
-					display_message("Thunder strikes! Location: " + fireTile["x"] / fireTile["pixel_size"] + ", " + fireTile["y"] / fireTile["pixel_size"]);
-				}
-			}
-        }, 1000);
+        let thunderTimeout;
+
+        function thunder() {
+            let rand = (Math.round(Math.random() * (30 - 10 + 1)) * 1000);
+            thunderTimeout = setTimeout(() => {
+                const flatten_arr = gameMap.flat();
+                let fireTile = flatten_arr[Math.floor(Math.random() * flatten_arr.length)];
+
+                for (let tile of getNeighbors(fireTile["x"] / fireTile["pixel_size"], fireTile["y"] / fireTile["pixel_size"])) {
+                    if (!tile.isDead) {
+                        setOnFire(tile);
+                        gameMap[tile.y / fireTile["pixel_size"]][tile.x / fireTile["pixel_size"]]["onFire"] = true;
+                    }
+                    display_message("Thunder strikes! Location: " + fireTile["x"] / fireTile["pixel_size"] + ", " + fireTile["y"] / fireTile["pixel_size"]);
+                }
+                thunder()
+            }, rand);
+        }
+
+        thunder();
         return function clearTime() {
             clearInterval(myTimer);
-            clearInterval(thunder);
+            clearInterval(thunderTimeout);
         }
     }, [speed, gameMap]);
 
@@ -98,9 +110,7 @@ const GameCanvas = () => {
 
     useEffect(() => {
         if (score === 30) {
-            display_message(
-                "New item unlocked! Helicopter: extinguish multiple fire at once"
-            );
+            display_message("New item unlocked! Helicopter: extinguish multiple fire at once");
         }
     }, [score]);
 
@@ -112,16 +122,11 @@ const GameCanvas = () => {
     }
 
     function getNeighbors(row, col) {
-        return [
-            col < 31 ? gameMap[row][col + 1] : gameMap[row][col],
-            col > 0 ? gameMap[row][col - 1] : gameMap[row][col],
-            row < 31 ? gameMap[row + 1][col] : gameMap[row][col],
-            row > 0 ? gameMap[row - 1][col] : gameMap[row][col],
-        ];
+        return [col < 31 ? gameMap[row][col + 1] : gameMap[row][col], col > 0 ? gameMap[row][col - 1] : gameMap[row][col], row < 31 ? gameMap[row + 1][col] : gameMap[row][col], row > 0 ? gameMap[row - 1][col] : gameMap[row][col],];
     }
 
-	function spreadFire(row, col) {
-		const neighbors = getNeighbors(row, col);
+    function spreadFire(row, col) {
+        const neighbors = getNeighbors(row, col);
 
         for (let tile of neighbors) {
             if (tile == null || tile.isDead) {
@@ -141,60 +146,47 @@ const GameCanvas = () => {
         }
     }
 
-	function burnTile(tile) {
-		if (!tile.isDead) {
-			tile.isDead = true
-			const ctx = canvasElem.current.getContext("2d");
+    function burnTile(tile) {
+        if (!tile.isDead) {
+            tile.isDead = true
+            const ctx = canvasElem.current.getContext("2d");
             let color = 'hsl(44, 0%, 21%)';
-            drawBorder(
-                tile.x,
-                tile.y,
-                tile.pixel_size,
-                tile.pixel_size,
-                color, ctx
-            );
+            drawBorder(tile.x, tile.y, tile.pixel_size, tile.pixel_size, color, ctx);
             ctx.fillStyle = color;
             ctx.fillRect(tile.x, tile.y, tile.pixel_size, tile.pixel_size);
 
-			if (tile['burnCd'] != null) {
-				clearInterval(tile['burnCd'])
-			}
+            if (tile['burnCd'] != null) {
+                clearInterval(tile['burnCd'])
+            }
 
-		}
-	}
+        }
+    }
 
-	function setOnFire(tile) {
-		const ctx = canvasElem.current.getContext("2d");
-		drawBorder(
-			tile.x,
-			tile.y,
-			tile.pixel_size,
-			tile.pixel_size,
-			"hsl(30, 82%, 56%)",
-			ctx
-		);
-		ctx.fillStyle = "rgb(235, 143, 52)";
-		ctx.fillRect(tile.x, tile.y, tile.pixel_size, tile.pixel_size);
-		
-		tile['burnCd'] = (setTimeout(()=> {
-				burnTile(tile)
-			}, 10000))
-		
-	}
+    function setOnFire(tile) {
+        const ctx = canvasElem.current.getContext("2d");
+        drawBorder(tile.x, tile.y, tile.pixel_size, tile.pixel_size, "hsl(30, 82%, 56%)", ctx);
+        ctx.fillStyle = "rgb(235, 143, 52)";
+        ctx.fillRect(tile.x, tile.y, tile.pixel_size, tile.pixel_size);
 
-	function waterOnFire(event) {
-		const pix_size = gameMap[0][0].pixel_size
-		let x = parseInt(event.nativeEvent.offsetX / pix_size );
-		let y = parseInt(event.nativeEvent.offsetY / pix_size);
-		if (score >= 30) {
-			extinguishFire(x,y)
-			for (let tile of getNeighbors(x, y)) {
-				extinguishFire(tile.y / pix_size, tile.x/pix_size);
-			}
-		} else {
-      		extinguishFire(x, y);
-		}
-	}
+        tile['burnCd'] = (setTimeout(() => {
+            burnTile(tile)
+        }, 10000))
+
+    }
+
+    function waterOnFire(event) {
+        const pix_size = gameMap[0][0].pixel_size
+        let x = parseInt(event.nativeEvent.offsetX / pix_size);
+        let y = parseInt(event.nativeEvent.offsetY / pix_size);
+        if (score >= 30) {
+            extinguishFire(x, y)
+            for (let tile of getNeighbors(x, y)) {
+                extinguishFire(tile.y / pix_size, tile.x / pix_size);
+            }
+        } else {
+            extinguishFire(x, y);
+        }
+    }
 
     function extinguishFire(x, y) {
         let tile = gameMap[y][x];
@@ -210,30 +202,22 @@ const GameCanvas = () => {
                 color = `hsl(41, 60%, 60%)`;
             }
 
-            drawBorder(
-                tile.x,
-                tile.y,
-                tile.pixel_size,
-                tile.pixel_size,
-                color,
-                ctx
-            );
+            drawBorder(tile.x, tile.y, tile.pixel_size, tile.pixel_size, color, ctx);
             ctx.fillStyle = color;
             ctx.fillRect(tile.x, tile.y, tile.pixel_size, tile.pixel_size);
             setScore(score + 1);
 
-			if (tile['burnCd'] != null) {
-				clearInterval(tile['burnCd'])
-			}
+            if (tile['burnCd'] != null) {
+                clearInterval(tile['burnCd'])
+            }
         }
     }
 
     const runOnTime = () => {
         const flatten_arr = gameMap.flat();
-        const randomTile =
-            flatten_arr[Math.floor(Math.random() * flatten_arr.length)];
+        const randomTile = flatten_arr[Math.floor(Math.random() * flatten_arr.length)];
 
-        const chance = (Math.round(Math.random() * 100)) / 100;
+        const chance = (Math.round(Math.random() * 50) + 1) / 100;
         let fireTile;
         if (chance <= new_igRate[randomTile.type] / 100 && !randomTile.onFire && !randomTile.isDead) {
             fireTile = randomTile;
@@ -258,33 +242,31 @@ const GameCanvas = () => {
         setSpeed(newSpeed);
     };
 
-    return (
-        <div className="container">
-            <h1>Save the Land</h1>
-            <canvas
-                ref={canvasElem}
-                className="gameCanvas"
-                onClick={waterOnFire}
-            ></canvas>
-            <div className="msg">
-                <h3>{message}</h3>
-            </div>
-            <div className="info">
-                <h2>Time: {timer.toFixed(1)}</h2>
-                <h2>Temperature: {temperature}</h2>
-                <h2>Speed: {speed}</h2>
-                <h2>Score: {score}</h2>
-            </div>
-            <div className="controls">
-                <button onClick={increaseSpeed} id="incSpeed">
-                    + Speed
-                </button>
-                <button onClick={decreaseSpeed} id="decSpeed">
-                    - Speed
-                </button>
-            </div>
+    return (<div className="container">
+        <h1>Save the Land</h1>
+        <canvas
+            ref={canvasElem}
+            className="gameCanvas"
+            onClick={waterOnFire}
+        ></canvas>
+        <div className="msg">
+            <h2>{message}</h2>
         </div>
-    );
+        <div className="info">
+            <h2>Time: {timer.toFixed(1)}</h2>
+            <h2>Temperature: {temperature}</h2>
+            <h2>Speed: {speed}</h2>
+            <h2>Score: {score}</h2>
+        </div>
+        <div className="controls">
+            <button onClick={increaseSpeed} id="incSpeed">
+                + Speed
+            </button>
+            <button onClick={decreaseSpeed} id="decSpeed">
+                - Speed
+            </button>
+        </div>
+    </div>);
 };
 
 export default GameCanvas;
