f996d037064fbc67f9d72535bdffb3e04feba3ce
Da_Dog <jiawenli233@outlook.com>
2022-10-08 22:18:54 -0700

    add current fires page margin top for all containers convert decimals to percentage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import './App.css'
 import GameCanvas from './components/GameCanvas';
 import InfoPage from './components/InfoPage';
 import HomePage from './components/HomePage'
 import RulePage from './components/RulePage';
+import CurrentFiresPage from "./components/CurrentFiresPage";
 
 
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav className='navbar'>
-          <Link to="/">Home</Link>
-          <Link to="/info">Info</Link>
-          <Link to="/game">Game</Link>
-          <Link to="/rule">Rule</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/info' element={<InfoPage/>}/>
-        <Route path='/game' element={<GameCanvas/>} />
-        <Route path='/rule' element={<RulePage/>} />
-      </Routes>
+    return (
+        <BrowserRouter>
+            <header>
+                <nav className='navbar'>
+                    <Link to="/">Home</Link>
+                    <Link to="/info">Info</Link>
+                    <Link to="/current-fires">Current Fires</Link>
+                    <Link to="/game">Game</Link>
+                    <Link to="/rule">Rule</Link>
+                </nav>
+            </header>
+            <Routes>
+                <Route path='/' element={<HomePage/>}/>
+                <Route path='/info' element={<InfoPage/>}/>
+                <Route path='/current-fires' element={<CurrentFiresPage/>}/>
+                <Route path='/game' element={<GameCanvas/>}/>
+                <Route path='/rule' element={<RulePage/>}/>
+            </Routes>
 
-    </BrowserRouter>
-  );
+        </BrowserRouter>
+    );
 }
 
 export default App;
